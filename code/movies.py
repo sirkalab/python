0a2e4577afa6fc568847ad75d49d8d18f64af274
Balakrishnan Devanasamy <sirkalab@gmail.com>
2018-05-01 07:21:46 -0500

    Triangle

--- code/movies.py
+++ code/movies.py
@@ -7,23 +7,34 @@
 
 import turtle
 
-def draw_square():
-    
+
+def draw_triangle_turtle(walk):
+    angle = 120
+    tilt = 3
+    count = 1
+    steps = 200
+    while (count < 120):
+        print(count)
+        if (count > 1):
+            walk.right(tilt)
+        walk.forward(steps)
+        walk.right(angle)
+        count = count + 1
+
+def draw_circle_turtle(walk):
+        walk.circle(100)
+
+
+def draw_turtles():
     window = turtle.Screen()
     window.bgcolor("red")
         
     walk = turtle.Turtle()
     walk.shape("turtle")
     walk.color("green")
-    walk.speed(1)
-    
-    walk.forward(100)
-    walk.right(120)
-    walk.forward(100)
-    walk.right(120)
-    walk.forward(100)
-
+    walk.speed(1000)
+    draw_triangle_turtle(walk)
+    draw_circle_turtle(walk)
     window.exitonclick()
-    
-    
-draw_square()    
+
+draw_turtles()
